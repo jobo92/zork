f0b01de20dc91b9034ce46f649851df911e1f07f
Unknown <marco.prolog@gmail.com>
2018-03-02 13:26:39 +0100

    After first lesson
    Made the Area class, a basic map, and reading the go command

---
+++ Zork/Area.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zork
+{
+    public enum Directions
+    {
+        West,
+        North,
+        South,
+        East
+    }
+
+    class Area
+    {
+        //something to hold connected nodes/areas
+        public Dictionary<Directions, Area> neighbors = new Dictionary<Directions, Area>();
+        //name of area
+        public string name;
+        //description of area
+        public string description;
+
+        //construtor
+        public Area(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+
+        //method to add an area to a specific direction
+        public void AddArea(Area other, Directions dir)
+        {
+            neighbors.Add(dir, other);
+
+            //other.neighbors.Add(, this);
+        }
+
+        //method that returns connected area in specific direction
+    }
+}
---
+++ Zork/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //defining nodes
+            Area a = new Area("A", "");
+            Area b = new Area("B", "");
+            Area c = new Area("C", "");
+            Area d = new Area("D", "");
+
+            //defining edges
+            a.AddArea(c, Directions.West);
+            c.AddArea(a, Directions.East);
+            c.AddArea(d, Directions.West);
+            d.AddArea(c, Directions.East);
+            a.AddArea(b, Directions.North);
+            b.AddArea(a, Directions.South);
+
+            //starting area
+            Area currentArea = a;
+
+            //read input
+            string command = Console.ReadLine();
+            //split it to separate, e.g. "go east" into {"go", "east"}
+            string[] inputs = command.Split(' ');
+            switch (inputs[0])
+            {
+                //the "go" command is given
+                case "go":
+                case "Go":
+                    switch (inputs[1])
+                    {
+                        case "east":
+                            GoToDirection(Directions.East, ref currentArea);
+                            break;
+                        case "west":
+                            GoToDirection(Directions.West, ref currentArea);
+                            break;
+                        case "south":
+                            GoToDirection(Directions.South, ref currentArea);
+                            break;
+                        case "north":
+                            GoToDirection(Directions.North, ref currentArea);
+                            break;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public static void GoToDirection(Directions dir, ref Area currentArea)
+        {
+            if (currentArea.neighbors.ContainsKey(dir))
+                currentArea = currentArea.neighbors[dir];
+            else Console.WriteLine("There's nothing there");
+        }
+    }
+}
